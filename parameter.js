a496dbcfc257f350c05c15f1f7b8215ad6732871
Sourav Sarker <souravsarker036@gmail.com>
2025-08-23 16:36:27 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,21 @@
+function vaatKhao(){
+    console.log('vat khete boso');
+}
+// vaatKhao();
+
+
+function squre (number){
+    // console.log(number);
+    console.log('value of the parameter', number)
+    const borgo = number * number
+    console.log('the multiply is: ', borgo)
+}
+squre(4);
+console.log('----------')
+squre(10)
+
+function add(a,b,c,d){
+    const total = a+b+c+d;
+    console.log(total);
+}
+add(2,4,6,8)
