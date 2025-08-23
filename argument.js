4e9c709591dee90e59f4853a8eec2206a284f1e4
Sourav Sarker <souravsarker036@gmail.com>
2025-08-23 16:58:00 +0600

    argument

---
+++ argument.js
@@ -0,0 +1,16 @@
+function doubleIt(number){
+    const doubled = number *2;
+    // console.log(number,doubled)
+}
+// console.log('call tha function')
+// doubleIt(15)
+// console.log('--------')
+// doubleIt(20)
+
+function difference(num1,num2){
+    const diff= num1 -num2;
+    console.log('differnce is: ',diff);
+}
+const fatherAge=45;
+const sonAge= 12;
+difference(fatherAge,sonAge)
